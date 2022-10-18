a24e95d9ad70fb53c078a215c3ea932aeccd91b6
zeMontreuilMITT <zacharie.montreuil@mitt.ca>
2022-10-18 09:58:17 -0500

    Add notes to Strategy

--- StrategyPattern/CompressionStrategyExample.cs
+++ StrategyPattern/CompressionStrategyExample.cs
@@ -5,6 +5,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// IDENTIFY an instance in which classes have some behaviours that change and some that will always remain the same
+// Compressor will always have a PerformCompression method that will take a Client ject
+// The behaviour of the PerformCompression method will change based on user input
+
+// STRATEGY
+// We must have at least one class that has an interface for a behaviour as a property
+// That interface needs to have at least one implementation
+// The class needs to invoke the method(s) of the interface
+// By runtime, an implementation of the interface needs to be chosen by the class
 
 Compressor compressor = new Compressor("zip");
 Client client = new Client();
@@ -35,6 +44,9 @@ namespace StrategyPattern
                     break;
                 case "zip":
                     CompressionBehaviour = new ZipCompressionBehaviour();
+                    break;
+                case "tar":
+                    CompressionBehaviour = new TarCompressionBehaviour();
                     break;
                 default:
                     throw new ArgumentException();
@@ -72,6 +84,15 @@ namespace StrategyPattern
         {
             Console.WriteLine($"Compressed {files.Count} into a zip file.");
             return $"zipped{files.Count}files.zip";
+        }
+    }
+
+    public class TarCompressionBehaviour: ICompressionBehaviour
+    {
+        public string Compress(List<string> files)
+        {
+            Console.WriteLine($"Tarring {files.Count} files");
+            return "compressedFiles.tar";
         }
     }
 }
