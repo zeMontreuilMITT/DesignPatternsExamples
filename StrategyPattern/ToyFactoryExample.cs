bf07144d22b14f624156a29f1ac674156a0e06bd
zeMontreuilMITT <zacharie.montreuil@mitt.ca>
2022-10-24 14:46:21 -0500

    Add basic "toy factory" solution example

---
+++ StrategyPattern/ToyFactoryExample.cs
@@ -0,0 +1,52 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+public abstract class ToyFactory
+{
+    public abstract Toy ManufactureToy(string type);
+
+    public void Paint(Toy toy)
+    {
+        Console.WriteLine($"Painting the {toy.Name}");
+    }
+    public Toy CreateToy(string type)
+    {
+        Toy toy;
+        toy = ManufactureToy(type);
+        Paint(toy);
+        Console.WriteLine("Toy's done");
+        return toy;
+    }
+}
+
+
+public class MattellFactory : ToyFactory
+{
+    public override Toy ManufactureToy(string type)
+    {
+        Toy toy;
+
+        switch (type)
+        {
+            case "car":
+                toy = new Car();
+                break;
+            default:
+                throw new ArgumentException();
+        } 
+
+        return toy;
+    }
+}
+public abstract class Toy
+{
+    public string Name { get; set; }
+}
+
+public class Car : Toy
+{
+    public Car()
+    {
+        Name = "Car";
+    }
+}
